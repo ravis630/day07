5f97612a0eaf5b133c736dd5255f5ded4a2d7289
ravis630 <ravishankarsharma333@gmail.com>
2022-12-28 22:49:46 +0530

    day07assignment

---
+++ DigitRepeatTwice.sh
@@ -0,0 +1,12 @@
+declare -a arr
+num=11
+index=0
+for (( i=11; i<100; i=$(( i + num )) ))
+do
+	#n=$(( $i / $num ))
+	#if [ $num -eq 0 ]
+	#then
+	arr[((index++))]=$i
+	#fi
+done
+echo "twice repeated digit numbers are: "${arr[*]}
